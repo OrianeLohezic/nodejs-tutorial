da91f472c4389678a583c085919da68a888692dc
hcapocci <hcapocci@talentsoft.com>
2019-04-04 14:38:29 +0200

    Add static files folder to express

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const app = require('./main');
+const express = require('express');
+
+app.use(express.static('public'));
 
 app.listen(process.env.APP_PORT, () => {
   console.log('Server listening on port:', process.env.APP_PORT);
